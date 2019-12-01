90ce23c0d9931d8e53671e80a4992dee80825dec
Robert Vari <mail.robertvari@gmail.com>
2019-12-01 17:13:32 +0100

    Profile with post_save signal

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200, blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pictures", blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    facebook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, settings.AUTH_USER_MODEL)
+def create_or_update_user_profile(sender, user_model, created, **kwargs):
+    if created:
+        Profile.objects.create(user=user_model)
+
+    user_model.profile.save()
--- Users/views.py
+++ Users/views.py
@@ -1,12 +1,15 @@
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/registration.html"
-    success_url = "/accounts/login/"
+    success_url = reverse_lazy("login")
 
 
-class ProfileView(TemplateView):
+class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "users/profile.html"
+    login_url = reverse_lazy("login")
