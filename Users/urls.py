2bbf86346f469e5ea4d66df267d956612a60db01
Robert Vari <mail.robertvari@gmail.com>
2019-12-01 16:41:07 +0100

    Profile form

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 
 urlpatterns = [
     path("accounts/", include('django.contrib.auth.urls')),
     path("signup/", SignUpView.as_view(), name="register"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -6,3 +6,7 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/registration.html"
     success_url = "/accounts/login/"
+
+
+class ProfileView(TemplateView):
+    template_name = "users/profile.html"
