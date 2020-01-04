f344a1cfde73e7ac2dad9272f2d08918261f3b08
Robert Vari <mail.robertvari@gmail.com>
2020-01-04 17:40:29 +0100

    Filtered posts view

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, PostDetailsView
+from .views import HomeView, PostDetailsView, PostFilterView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('posts/', PostFilterView.as_view(), name='post_filters'),
     path('posts/<str:slug>', PostDetailsView.as_view(), name='post_details'),
 ]
--- Pages/views.py
+++ Pages/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView, DetailView
-
+from django.views.generic import TemplateView, DetailView, ListView
+from django.db.models import Count
 from Users.models import Post
 
 
@@ -10,6 +10,27 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context["top_row_list"] = Post.objects.all().filter(picked=True)
         return context
+
+
+class PostFilterView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        sorting = self.request.GET.get('sorting')
+
+        if sorting == 'community':
+            return self.model.objects.annotate(like_count=Count('likes')).order_by('-like_count')
+
+        elif sorting == 'trending':
+            return self.model.objects.annotate(comment_count=Count('comments')).order_by('-comment_count')
+
+        elif sorting == 'picks':
+            return self.model.objects.all().filter(picked=True)
+
+        else:
+            return self.model.objects.all()
 
 
 class PostDetailsView(DetailView):
