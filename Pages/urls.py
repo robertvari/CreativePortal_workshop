6eef3098d84f29d2a093ea3ac819433f216c4617
Robert Vari <mail.robertvari@gmail.com>
2020-01-04 15:47:09 +0100

    Post details view

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, PostDetailsView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('posts/<str:slug>', PostDetailsView.as_view(), name='post_details'),
 ]
--- Pages/views.py
+++ Pages/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from Users.models import Post
 
 
 class HomeView(TemplateView):
@@ -6,5 +8,11 @@ class HomeView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context["top_row_list"] = []
+        context["top_row_list"] = Post.objects.all().filter(picked=True)
         return context
+
+
+class PostDetailsView(DetailView):
+    model = Post
+    template_name = 'users/post_details.html'
+    context_object_name = "post"
